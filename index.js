ba9bcf9d9aacf67e11a3c1b30a8c72242020d2fc
romlareo <rglareo@gmail.com>
2018-04-20 01:42:47 -0300

    agrego index para js y modificaciones

---
+++ index.js
@@ -0,0 +1,34 @@
+
+$(function(){
+
+    $('a[href*=#]').click(function() {
+
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        && location.hostname == this.hostname) {
+
+            var $target = $(this.hash);
+
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+
+            if ($target.length) {
+
+                var targetOffset = $target.offset().top;
+
+                $('html,body').animate({scrollTop: targetOffset}, 1000);
+
+                return false;
+
+           }
+
+      }
+
+  });
+
+});
+
+var boton = document.getElementById("myBtn");
+
+function menuDesplegable (menu){
+    document.getElementById("myDropdown").classList.toggle("show"); 
+}
+boton.addEventListener("click",menuDesplegable);
